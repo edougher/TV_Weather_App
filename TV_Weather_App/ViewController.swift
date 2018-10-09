3a4f2f37919208fca4bce707ea3cc5d4dd4a8921
Erin Dougher <edougher@students.mchenry.edu>
2018-10-08 19:13:30 -0500

    Initial Commit

---
+++ TV_Weather_App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TV_Weather_App
+//
+//  Created by Aaron Dougher on 10/8/18.
+//  Copyright © 2018 Erin Dougher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
