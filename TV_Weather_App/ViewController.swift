c77549381ceb65e5ee65c0bce2fcc0b1ff7ffd2d
Erin Dougher <edougher@students.mchenry.edu>
2018-10-17 14:03:08 -0500

    try again

--- TV_Weather_App/ViewController.swift
+++ TV_Weather_App/ViewController.swift
@@ -12,9 +12,40 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let newUrl = NSURL(string: "https://api.darksky.net/forecast/707832b21ca30e8b4edb6193c3885717/42.2411,-88.3162"){
+        
+        if let data = NSData(contentsOf: newUrl as URL){
+            
+            
+            do {
+                let parsed = try JSONSerialization.jsonObject(with: data as Data, options: JSONSerialization.ReadingOptions.allowFragments) as! [String: AnyObject]
+                
+                let newDict = parsed
+                print(newDict["currently"]!["summary"] as Any)
+                print(newDict["currently"]!["temperature"] as Any)
+                
+                self.summary.text =  "\(newDict["currently"]!["summary"]!!)"
+                
+                self.temp.text = "\(newDict["currently"]!["temperature"]!!)"
+                
+                self.apparentTemp.text = "\(newDict["currently"]!["apparentTemperature"]!!)"
+                
+            }
+            catch let error as NSError {
+                print("A JSON parsing error occurred, here are the details:\n \(error)")
+            }
+        }
+    }
+    
     }
 
-
+  
+    
+    @IBOutlet weak var summary: UILabel!
+    
+    @IBOutlet weak var temp: UILabel!
+    
+    @IBOutlet weak var apparentTemp: UILabel!
 }
 
